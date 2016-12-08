5e549f429f87f4165352581a07edcdfc0bd6475f
andla306 <andla306@student.liu.se>
2016-12-08 20:06:56 +0100

    Created the models for the poll app

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,5 +1,11 @@
 from __future__ import unicode_literals
-
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published') # You can use the first positional argument to a field to designate a human-readable name.
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
